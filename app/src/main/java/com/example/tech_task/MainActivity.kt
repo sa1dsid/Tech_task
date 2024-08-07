3d291e8f7b7f3c268ca3cd00e7e82e292c95d5e1
sa1dsid <worming.said@gmail.com>
2024-08-07 18:19:17 +0300

    Initial commit

---
+++ app/src/main/java/com/example/tech_task/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.example.tech_task
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val btnNavView = findViewById<BottomNavigationView>(R.id.bottomNav)
+        val controller = findNavController(R.id.fragmentContainerView)
+        btnNavView.setupWithNavController(controller)
+    }
+}
